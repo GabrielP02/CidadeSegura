a1de511dc9d42b3b1fc90019e721ace9540dd063
GabrielP02 <115106954+GabrielP02@users.noreply.github.com>
2024-05-20 11:26:46 -0300

    commit1

---
+++ src/routes/Entrar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './fontes.css';
+import './Entrar.css';
+import Login from "../assets/components/Login/Login";
+
+
+const Entrar = () => {
+return (
+    <div class="Entrar">
+        <Login/>
+    </div>
+   
+)
+
+}
+export default Entrar;
