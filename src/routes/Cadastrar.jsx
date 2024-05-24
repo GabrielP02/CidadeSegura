72f611d551b94c1b13141f3e2affbe34c62248e3
GabrielP02 <115106954+GabrielP02@users.noreply.github.com>
2024-05-24 13:45:44 -0300

    Atualização

---
+++ src/routes/Cadastrar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './fontes.css';
+import './Cadastrar.css';
+import Cadastro from "../assets/components/Cadastro/Cadastro";
+
+const Cadastrar = () => {
+return (
+    <div class="Cadastrar">
+        <Cadastro/>
+
+    </div>
+)
+
+}
+export default Cadastrar;
